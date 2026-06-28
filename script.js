dc5c64038fd3e60d9cdd541cc8c56d7ba0841623
Yeferson Chunga <yefersonchunga@gmail.com>
2026-06-27 20:05:10 -0500

    Actualizo datos del laboratorio

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(Hola desde javascript!);
+let nombre = 'Yeferson';
+let edad = 28;
+let programador = true;
+
+console.log(nombre, typeof nombre);
+console.log(edad, typeof edad);
+console.log(programador, typeof programador);
+
+let msg1 =
+"Hola, soy " +
+nombre +
+", tengo " +
+edad +
+" años y soy programador";
+
+console.log(msg1);
+
+let msg2 = `Hola, soy ${nombre}, tengo ${edad} años y soy programador`;
+
+console.log(msg2);
