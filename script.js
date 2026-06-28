98f465b1f9ff38e7bdfb3d82211be85a05fcd85b
Yeferson Chunga <yefersonchunga@gmail.com>
2026-06-27 22:37:58 -0500

    condicionales js - clase 10

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-let nombre = 'Yeferson';
-let edad = 28;
-let programador = true;
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
+let intentos = 3;
+let numeroUsuario;
 
-console.log(nombre, typeof nombre);
-console.log(edad, typeof edad);
-console.log(programador, typeof programador);
+console.log("Juego iniciado por Yeferson");
+console.log("Debes adivinar un número del 1 al 10");
 
-let msg1 =
-"Hola, soy " +
-nombre +
-", tengo " +
-edad +
-" años y soy programador";
+while (intentos > 0) {
+    numeroUsuario = Number(
+        prompt(`Adivina el número del 1 al 10. Tienes ${intentos} intentos:`)
+    );
 
-console.log(msg1);
+    console.log(`Yeferson ingresó el número: ${numeroUsuario}`);
 
-let msg2 = `Hola, soy ${nombre}, tengo ${edad} años y soy programador`;
+    if (numeroUsuario === numeroSecreto) {
+        alert(`¡Correcto! Adivinaste el número ${numeroSecreto}`);
+        console.log(`¡Yeferson ganó! El número secreto era ${numeroSecreto}`);
+        break;
+    } else if (numeroUsuario < numeroSecreto) {
+        alert("El número secreto es mayor");
+        console.log("El número ingresado es menor que el número secreto");
+    } else {
+        alert("El número secreto es menor");
+        console.log("El número ingresado es mayor que el número secreto");
+    }
 
-console.log(msg2);
+    intentos--;
+}
+
+if (numeroUsuario !== numeroSecreto) {
+    alert(`Perdiste. El número secreto era ${numeroSecreto}`);
+    console.log(`Yeferson perdió. El número secreto era ${numeroSecreto}`);
+}
